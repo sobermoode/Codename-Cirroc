d3a27fbd413fb6734f3aa4ba18fa2039bf9bfc6a
Aaron Justman <sobermoode@yahoo.com>
2017-06-27 14:37:53 -0700

    The game now starts with.a "Press START" screen, which disappears when the button is pressed. The Start button then changes to the Restart button.

---
+++ CaribooInitialTest/Gameboard.swift
@@ -0,0 +1,17 @@
+//
+//  Gameboard.swift
+//  CaribooInitialTest
+//
+//  Created by Aaron Justman on 6/27/17.
+//  Copyright © 2017 Aaron Justman. All rights reserved.
+//
+
+import SpriteKit
+
+class Gameboard: SKScene {
+    
+    /*override func didMove(to view: SKView) {
+        let cover = childNode(withName: "cover") as! SKSpriteNode
+        cover.alpha = 0
+    }*/
+}
---
+++ CaribooInitialTest/StartButton.swift
@@ -0,0 +1,31 @@
+//
+//  StartButton.swift
+//  CaribooInitialTest
+//
+//  Created by Aaron Justman on 6/27/17.
+//  Copyright © 2017 Aaron Justman. All rights reserved.
+//
+
+import SpriteKit
+
+class StartButton: SKSpriteNode {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        isUserInteractionEnabled = true
+    }
+}
+
+extension StartButton {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        buttonPress(self)
+    }
+}
+
+extension StartButton: ButtonNode {
+    func buttonPress(_ pressedButton: ButtonNode) {
+        scene!.childNode(withName: "cover")!.removeFromParent()
+        run(SKAction.setTexture(SKTexture(imageNamed: "restartButton")))
+    }
+}
