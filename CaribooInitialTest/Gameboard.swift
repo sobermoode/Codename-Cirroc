be8213652007b909a8b300a606ee1afcfc4af38a
Aaron Justman <sobermoode@yahoo.com>
2017-06-29 13:57:04 -0700

    Fixed the issue retrieving the reference's actual node. All the picture boxes get a random image, now.

--- CaribooInitialTest/Gameboard.swift
+++ CaribooInitialTest/Gameboard.swift
@@ -10,9 +10,32 @@ import SpriteKit
 
 class Gameboard: SKScene {
     
-    /*override func didMove(to view: SKView) {
-        //let cover = childNode(withName: "cover") as! SKSpriteNode
-        //cover.alpha = 0
-        print("Gameboard didMove(to:")
-    }*/
+    override func didMove(to view: SKView) {
+        setup()
+    }
+    
+    func setup() {
+        let currentThemeTextures = SKTextureAtlas(named: PictureBox.Theme.Animals)
+        var currentThemeNames = currentThemeTextures.textureNames
+        
+        let pictureBoxes = childNode(withName: "pictureBoxes")!.children as! [SKReferenceNode]
+        for box in pictureBoxes {
+            if let outer = box.children.first {
+                if let pictureBoxNode = outer.children.first {
+                    let rando = Int(arc4random_uniform(UInt32(currentThemeNames.count)))
+                    let themeName = currentThemeNames.remove(at: rando)
+                    let texture = currentThemeTextures.textureNamed(themeName) //SKTexture(imageNamed: PictureBox.currentThemeNames![rando])
+                    if let picture = pictureBoxNode.childNode(withName: "picture") as? SKSpriteNode {
+                        picture.run(SKAction.setTexture(texture))
+                    } else {
+                        print("couldn't change the picture for", box.name!)
+                    }
+                } else {
+                    print("couldn't get the pictureBoxNode for", box.name!)
+                }
+            } else {
+                print("couldn't get the outer for", box.name!)
+            }
+        }
+    }
 }
